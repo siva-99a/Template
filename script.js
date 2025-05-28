35fb233e416512c19d9c15edbfaf1ddee8517931
siva-99a <132882048+siva-99a@users.noreply.github.com>
2025-05-28 22:58:44 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const hamburger = document.getElementById("hamburger");
-const mobileMenu = document.getElementById("mobileMenu");
-
-hamburger.addEventListener("click", () => {
-  mobileMenu.classList.toggle("open");
-  // Toggle aria-label for accessibility
-  const isOpen = mobileMenu.classList.contains("open");
-  hamburger.setAttribute("aria-label", isOpen ? "Close Menu" : "Open Menu");
-});
